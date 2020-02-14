969f37bbb20f24c3938a8bc21d70c6aea7c55246
NaggiGoishi <naggigoishi@gmail.com>
2020-02-13 22:34:16 -0800

    set Content-Security-Policy

--- app.rb
+++ app.rb
@@ -5,3 +5,7 @@ get '/' do
   sheet = SpreadSheet.new
   erb :index, locals: {'rows': sheet.rows }
 end
+
+after do
+  headers({'Content-Security-Policy' => "frame-ancestors 'self' kakaxi.me kakaxi.jp flag.dispbox.com"})
+end
